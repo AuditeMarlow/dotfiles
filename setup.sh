0ea0113690d6fc3a1e8a34d740f8db49fc60f74f
Niek den Breeje <n.j.den.breeje@gmail.com>
2023-03-14 18:04:42 +0100

    feat: install tooling and dependencies in setup

--- setup.sh
+++ setup.sh
@@ -24,4 +24,18 @@ dots pull origin main
 dots branch --set-upstream-to origin/main main
 rm "$HOME/README.md" "$HOME/setup.sh" &> /dev/null || true
 
-echo -e "\\033[0;32mSuccessfully set up dotfiles\\033[0m"
+echo -e "\\033[0;32mSuccessfully set up dotfiles!\\033[0m"
+echo
+echo -e "\\033[0;32mProceeding to install tooling and dependencies\\033[0m"
+
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+mkdir -p ~/.local/share/zsh
+curl -L git.io/antigen > ~/.local/share/zsh/antigen.zsh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+echo -e "\\033[0;32mSuccessfully installed tooling and dependencies\\033[0m"
+echo
+echo -e "\\033[0;32mDone!\\033[0m"
