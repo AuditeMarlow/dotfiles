6d4b2707be37a51cdf5107fdcf5feec1ba23e0ed
Niek den Breeje <n.denbreeje@guardian360.nl>
2025-10-30 10:29:18 +0100

    feat: add codecompanion.nvim

---
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,32 @@
+local M = {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            config = function()
+                local treesitter = require("nvim-treesitter.configs")
+
+                treesitter.setup({
+                    ensure_installed = {
+                        "lua",
+                        "markdown",
+                        "markdown_inline",
+                        "yaml",
+                    }
+                })
+            end
+        },
+    },
+    opts = {},
+}
+
+vim.keymap.set({"n", "v"}, "<c-a>", "<cmd>CodeCompanionActions<cr>")
+vim.keymap.set({"n", "v"}, "<leader>cc", "<cmd>CodeCompanionChat Toggle<cr>")
+vim.keymap.set("v", "<leader>ca", "<cmd>CodeCompanionChat Add<cr>")
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanion]])
+
+return M
